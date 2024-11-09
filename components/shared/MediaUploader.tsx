47ce2e868e943b68f93fcb88c7baaa992568a774
proshaban <shaban.khan.self@gmail.com>
2024-11-09 22:34:05 +0530

    updating clounary presists

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="proshaban_imagefai"
       options={{
         multiple: false,
         resourceType: "image",
